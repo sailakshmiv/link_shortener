b268c8cb3f3065567c1094602824beb1fa93690c
irmiller22 <irmiller22@gmail.com>
2014-02-02 16:39:16 -0500

    finalized component of link shortener app via controller -- update_params

--- app/controllers/urls_controller.rb
+++ app/controllers/urls_controller.rb
@@ -4,7 +4,7 @@ class UrlsController < ApplicationController
   end
 
   def create 
-  	@short_url = Url.new(params[:url])
+  	@short_url = Url.new(url_params)
   	if @short_url.save
   		flash[:short_id] = @short_url.id
   		redirect_to new_url_url
@@ -16,5 +16,11 @@ class UrlsController < ApplicationController
   def show
   	@short_url = Url.find(params[:id])
   	redirect_to @short_url.url
+  end
+
+  private
+
+  def url_params
+  	params.require(:url).permit(:url)
   end
 end
